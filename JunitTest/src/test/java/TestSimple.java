e966e0b01dca336bcb509e57270d647e761302cf
ManuelPoot <manuel.poot753@gmail.com>
2020-03-12 14:17:10 -0600

    Added params tests

--- JunitTest/src/test/java/TestSimple.java
+++ JunitTest/src/test/java/TestSimple.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.opentest4j.AssertionFailedError;
 import steps.DemoInputField;
 
 import java.util.logging.Logger;
 
 public class TestSimple {
-    static final Logger logger = Logger.getLogger(TestSimple.class.getName());
+    private static final Logger logger = Logger.getLogger(TestSimple.class.getName());
 
     @BeforeAll
     public static void setupAll(){
@@ -30,6 +33,31 @@ public class TestSimple {
             demo.showMessage();
             throw new NullPointerException();
         });
+    }
+
+    @ParameterizedTest
+    @ValueSource ( strings = {" an it a lava la tina", "hola", "olo"})
+    public void idPalindrome (String word) {
+        boolean condition = true;
+
+        word = word.trim().replace(" ", "");
+        int word_length = word.length();
+
+        for(int i = 0; i < word_length/2; i++){
+            //System.out.println(String.format("%s, %s", word.charAt(i), word.charAt(word_length - (i + 1))));
+            if (word.charAt(i) != word.charAt(word_length - (i + 1))) {
+                condition = false;
+                break;
+            }
+        }
+
+        try{
+            Assertions.assertTrue(condition, String.format("'%s' is not palindrome", word));
+            logger.info(String.format("'%s' is palindrome", word));
+        }catch (AssertionFailedError e){
+            logger.info(e.getMessage());
+            throw e;
+        }
     }
 
     @AfterEach
