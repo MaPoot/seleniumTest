a601f7c9927c34501d22207bd2d86f753cd590de
ManuelPoot <48805159+kurokami753@users.noreply.github.com>
2020-10-01 17:20:54 -0500

    Update PageObject

--- Selenium/src/main/java/pages/PageObject.java
+++ Selenium/src/main/java/pages/PageObject.java
@@ -1,10 +1,16 @@
 package pages;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 public abstract class PageObject {
     protected WebDriver driver;
@@ -22,4 +28,17 @@ public abstract class PageObject {
         WebDriverWait wait = new WebDriverWait(driver, 30);
         wait.until(pageLoadCondition);
     }
+
+    public static boolean waitURLChange(WebDriver driver, String currentUrl, int timeout){
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(timeout))
+                .pollingEvery(Duration.ofSeconds(2))
+                .ignoreAll(Arrays.asList(NoSuchElementException.class, StaleElementReferenceException.class));
+
+        return wait.until(driver1 -> {
+           String newURL = driver1.getCurrentUrl();
+           return !newURL.equals(currentUrl);
+        });
+    }
+
 }
